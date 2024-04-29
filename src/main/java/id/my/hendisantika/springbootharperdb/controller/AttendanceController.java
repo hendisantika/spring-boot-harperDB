a14f7b3457b2d543d3da67f61c689e304fdaec74
hendisantika <hendisantika@yahoo.co.id>
2024-04-29 11:06:37 +0700

    Add editLeave in AttendanceController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
+++ src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
@@ -1,12 +1,14 @@
 package id.my.hendisantika.springbootharperdb.controller;
 
 import id.my.hendisantika.springbootharperdb.dto.EmployeeDTO;
+import id.my.hendisantika.springbootharperdb.dto.EmployeeEditDataDTO;
 import id.my.hendisantika.springbootharperdb.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,6 +45,11 @@ public class AttendanceController {
     @PostMapping(value = "/api/add/leave")
     public HashMap<String, String> addNewLeave(@RequestBody EmployeeDTO employeeData) {
         return attendanceService.addNewLeaveForEmployee(employeeData);
+    }
+
+    @PutMapping(value = "/api/edit/leave")
+    public HashMap<String, String> editLeave(@RequestBody EmployeeEditDataDTO employeeEditData) {
+        return attendanceService.editLeaveForEmployee(employeeEditData);
     }
 }
 
