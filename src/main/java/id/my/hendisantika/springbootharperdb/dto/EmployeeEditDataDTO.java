fc2289e03252fe5cf24e8e4ab5610a895e7a82fc
hendisantika <hendisantika@yahoo.co.id>
2024-04-29 10:42:26 +0700

    Add EmployeeEditDataDTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootharperdb/dto/EmployeeEditDataDTO.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.springbootharperdb.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-harperDB
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/29/24
+ * Time: 10:42
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class EmployeeEditDataDTO {
+    private String employeeId;
+    private String previousDate;
+    private String newDate;
+}
