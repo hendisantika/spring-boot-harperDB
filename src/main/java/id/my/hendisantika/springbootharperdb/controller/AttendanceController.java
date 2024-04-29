3bcfe0a3f01fc159985b0f6d63c6d39c4930f147
hendisantika <hendisantika@yahoo.co.id>
2024-04-29 10:46:26 +0700

    Add healthCheck in AttendanceController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
+++ src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.springbootharperdb.controller;
 import id.my.hendisantika.springbootharperdb.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,4 +23,8 @@ public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
+    @GetMapping("/")
+    public String healthCheck() {
+        return "OK";
+    }
 }
