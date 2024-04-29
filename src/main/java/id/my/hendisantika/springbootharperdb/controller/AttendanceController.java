26bd3a3c5fb02f6b38cabb2e3fb3a0edbf1f7af3
hendisantika <hendisantika@yahoo.co.id>
2024-04-29 11:06:59 +0700

    Add cancelLeave in AttendanceController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
+++ src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
@@ -5,6 +5,7 @@ import id.my.hendisantika.springbootharperdb.dto.EmployeeEditDataDTO;
 import id.my.hendisantika.springbootharperdb.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,11 @@ public class AttendanceController {
     @PutMapping(value = "/api/edit/leave")
     public HashMap<String, String> editLeave(@RequestBody EmployeeEditDataDTO employeeEditData) {
         return attendanceService.editLeaveForEmployee(employeeEditData);
+    }
+
+    @DeleteMapping(value = "/api/cancel/leave")
+    public HashMap<String, String> cancelLeave(@RequestBody EmployeeDTO employeeData) {
+        return attendanceService.cancelLeaveForEmployee(employeeData);
     }
 }
 
