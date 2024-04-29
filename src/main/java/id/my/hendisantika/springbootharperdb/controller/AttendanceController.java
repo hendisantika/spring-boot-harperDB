40fae43df4197e85d967ff80001dd3f64ea5da7c
hendisantika <hendisantika@yahoo.co.id>
2024-04-29 10:46:14 +0700

    Add AttendanceController 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.springbootharperdb.controller;
+
+import id.my.hendisantika.springbootharperdb.service.AttendanceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-harperDB
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/29/24
+ * Time: 10:45
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AttendanceController {
+
+    private final AttendanceService attendanceService;
+
+}
