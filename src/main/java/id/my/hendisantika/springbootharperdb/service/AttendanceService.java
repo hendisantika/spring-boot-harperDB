c70382024ea11e23f60d9f65bd1a0e57adb79150
hendisantika <hendisantika@yahoo.co.id>
2024-04-29 10:43:51 +0700

    Add AttendanceService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootharperdb/service/AttendanceService.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.springbootharperdb.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-harperDB
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/29/24
+ * Time: 10:43
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AttendanceService {
+
+    private final ConnectionService connectionService;
+
+}
