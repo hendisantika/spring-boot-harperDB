3724f33ab97a535abd8b73593fcba976759d3ebe
hendisantika <hendisantika@yahoo.co.id>
2024-04-29 11:06:23 +0700

    Add addNewLeave in AttendanceController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
+++ src/main/java/id/my/hendisantika/springbootharperdb/controller/AttendanceController.java
@@ -1,10 +1,13 @@
 package id.my.hendisantika.springbootharperdb.controller;
 
+import id.my.hendisantika.springbootharperdb.dto.EmployeeDTO;
 import id.my.hendisantika.springbootharperdb.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -36,4 +39,10 @@ public class AttendanceController {
     public List<HashMap<String, String>> getAllLeavesForEmployee(@PathVariable String employeeId) {
         return attendanceService.getAllLeavesForEmployee(employeeId);
     }
+
+    @PostMapping(value = "/api/add/leave")
+    public HashMap<String, String> addNewLeave(@RequestBody EmployeeDTO employeeData) {
+        return attendanceService.addNewLeaveForEmployee(employeeData);
+    }
 }
+
