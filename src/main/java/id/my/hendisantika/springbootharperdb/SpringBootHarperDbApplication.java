2bdc6d29c4ab03567d15aad34f4491f2e1af3b91
hendisantika <hendisantika@yahoo.co.id>
2024-04-29 10:39:43 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootharperdb/SpringBootHarperDbApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootharperdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHarperDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHarperDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootharperdb/SpringBootHarperDbApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootharperdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHarperDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
